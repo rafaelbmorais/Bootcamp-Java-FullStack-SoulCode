dbe2785e5de9ecf9a0ef791fca94723801568652
rafaelbmorais <morais.rafael@outlook.com>
2023-07-12 13:01:28 -0300

    Atualizado o conteúdo da aula em 12/07/23.

--- estudando-js/aula-120723/funcoes-anonimas-arrows.js
+++ estudando-js/aula-120723/funcoes-anonimas-arrows.js
@@ -1,8 +1,8 @@
-// CALLBACK
-function requisicaoParaBanco(callbackError) { // callbackError é um apelido para a função que é passado como argumento
+// ==== CALLBACK
+function requisicaoParaBanco(callbackError) { // callbackError é um apelido para a função que é passada como argumento
     for(let i = 0; i < 10000000000; i++) { // requisição feita para o servidor (simulação)
         if(i === 1000000) { // irá gerar um erro
-            callbackError(); // chama uma função que executará o que foi estipulado para tratar esse erro
+            callbackError(); // chama uma função que executará o que foi estipulado para tratar esse erro, no caso function erro(){}
         }
     }
 }
@@ -13,15 +13,47 @@ function erro() {
 
 // requisicaoParaBanco(erro);
 
-// =========
+// =================================================================
 
 
-// FUNÇÃO ANÔNIMA
+// ===== FUNÇÃO ANÔNIMA
+// function {
+// }
+
+// essa é uma função normal que será usada como exemplo do callback com função anônima
 function teste() {
     console.log("Olá, Mundo!");
 }
 
-teste();
-
+// aqui é chamada a função setTimeout, usando como um dos argumentos a função teste
 // setTimeout(teste, 5000);
+
+// essa função abaixo, chamada de Função Anônima, é a mesma que a de cima - função teste(), mas de uma forma reduzida. 
+// Ela é dessa forma quando você precisa utilizar apenas uma única vez!
+setTimeout(function() {
+    console.log("Olá, Mundo!");
+}, 5000);
+
+// aqui é chamada a função setInterval, usando como um dos argumentos a função teste
 // setInterval(teste, 2000);
+
+// essa função abaixo, chamada de Função Anônima, é a mesma que a de cima - função teste(), mas de uma forma reduzida. 
+// Ela é dessa forma quando você precisa utilizar apenas uma única vez!
+setInterval(function(){
+    console.log("Olá, Mundo usando setInterval")
+}, 2000);
+
+// ================================================================+
+
+// ===== ARROW FUNCTIONS ()=> (Funções de Seta, Funções Anônimas)
+
+// Essa é uma função Anônima
+setTimeout(function() {
+    console.log("SetTimeout com Função Anônima");
+}, 2000);
+
+
+// Essa é uma Função Arrow, porque usa somente os parenteses e =>
+setTimeout(() => {
+    console.log("SetTimeout com Função Arrow");
+}, 2000);
